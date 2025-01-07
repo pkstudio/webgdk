141ead280b0e1a2c0549e567938f07123aa75402
Marcus Alexander Dahl <marcus@programkode.studio>
2025-01-07 14:44:32 +0100

    update dependencies

--- frontend/sandbox/public/files/js/engine.js
+++ frontend/sandbox/public/files/js/engine.js
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2024  Marcus Alexander Dahl <@programkode>
+Copyright (C) 2025  Marcus Alexander Dahl <@programkode>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
